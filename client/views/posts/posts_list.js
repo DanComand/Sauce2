31b0061ad99d2477bcb06937fc02132b4fad3a6e
Dan Comand <dcomand@gmail.com>
2015-11-22 18:42:35 -0500

    removed dummy data from posts_list.js

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,22 +1,5 @@
-var postsData = [
-  {
-    title: 'introducing telescope',
-    author: 'sacha greif',
-    url: 'http://sachagreif.com/introducing-telescope'
-  },  
-
-  {
-    title: 'meteor',
-    author: 'tom coleman',
-    url: 'http://meteor.com'
-  },  
-
-  {
-    title: 'the meteor book',
-    author: 'tom coleman',
-    url: 'http://themeteorbook.com'
-  },
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
