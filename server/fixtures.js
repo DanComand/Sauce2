781a307535284fd0ced3f61dc485516a42d0166a
Dan Comand <dcomand@gmail.com>
2015-11-22 18:38:55 -0500

    added posts inserts to be loaded on the server.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,1 +1,19 @@
-if 
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'introducing telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope'
+  });
+
+  Posts.insert({
+    title: 'meteor',
+    author: 'tom coleman',
+    url: 'http://meteor.com'
+  });
+
+  Posts.insert({
+    title: 'meteor book',
+    author: 'tom COLEMAN',
+    url: 'http://themeteorbook.com'
+  });
+}
